dd525294c0fc4f86c93ac2262f971f4b6321e3c0
TPWS2122-MLBCI-MF <moritz.fesseler@student.uni-tuebingen.de>
2023-01-22 23:17:25 +0100

    added pics

--- python_ML4.py
+++ python_ML4.py
@@ -16,15 +16,15 @@ def main():
     val_data = ig.LoadImageDataFromFile(":\Users\Moritz\Desktop\Allgemeines\MachineLearning\rps", WIDTH, HEIGHT)
 
 
-    TRAINING_DIR = "C:/Users/LS_MFE/Desktop/rps"
-    VALIDATION_DIR = "C:/Users/LS_MFE/Desktop/rps-test-set"
-    training_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255)
-    validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255)
+    #TRAINING_DIR = "C:/Users/LS_MFE/Desktop/rps"
+    #VALIDATION_DIR = "C:/Users/LS_MFE/Desktop/rps-test-set"
+    #training_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255)
+    #validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1./255)
 
-    train_generator = training_datagen.flow_from_directory(TRAINING_DIR, target_size=(150,150), class_mode='categorical')
-    validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR, target_size=(150,150), class_mode='categorical')
+    #train_generator = training_datagen.flow_from_directory(TRAINING_DIR, target_size=(150,150), class_mode='categorical')
+    #validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR, target_size=(150,150), class_mode='categorical')
     
-    validataion_dataset_paper = validation_datagen.flow_from_directory("YOUR PATH TO PAPER ONLY DATA, FOR TESTING PURPOSES", target_size=(150,150), class_mode='categorical')
+    #validataion_dataset_paper = validation_datagen.flow_from_directory("YOUR PATH TO PAPER ONLY DATA, FOR TESTING PURPOSES", target_size=(150,150), class_mode='categorical')
     
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Conv2D(64, (3,3) , activation='relu', input_shape=(WIDTH, HEIGHT,3)))
