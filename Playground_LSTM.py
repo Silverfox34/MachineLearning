092d395d579a7d7bb6041bf32fb8e8646e0b9469
TPWS2122-MLBCI-MF <moritz.fesseler@student.uni-tuebingen.de>
2023-02-15 20:40:34 +0100

    update

--- Playground_LSTM.py
+++ Playground_LSTM.py
@@ -1,36 +1,44 @@
 import numpy as np
 from keras.models import Sequential
 from sklearn.model_selection import train_test_split
+from keras.layers import LSTM as LSTM
 
 def main():
     time_steps = 3
-    data = np.array([[4,3,3.5,4], 
-                     [1,1.25,1.3,1.2],
-                     [5,4,5,3],
-                     [1,2,3,2],
-                     [1,5,3,4], 
-                     [1,5,2,3],
-                     [7,3,1,3],
-                     [9,6,5,4],
-                     [3,1,2,3],
-                     [5, 7.312, 3, 6]])
+    data = np.array([
+        
+                    [[1,2,3, 2,3,4, 3,4,5, 4,5,6],
 
-    
+                    [10,11,12,11,12,13,12,13,14,13,14,15]],
+                    
+                    [[1,2,3, 2,3,4, 3,4,5, 4,5,6],
 
-    sequences = []
+                    [10,11,12,11,12,13,12,13,14,13,14,15]],
 
-    for i in range(0, len(data) - time_steps):
-        sequence = data[i : i+time_steps]
-        sequences.append(sequence)
+                    [[1,2,3, 2,3,4, 3,4,5, 4,5,6],
+
+                    [10,11,12,11,12,13,12,13,14,13,14,15]]
+                    
+                    ])
+    print(data.shape)
+
+    return
+
+    #sequences = []
+
+    #for i in range(0, len(data) - time_steps):
+        #sequence = data[i : i+time_steps]
+        #sequences.append(sequence)
     
-    sequences = np.array(sequences)
-    print(sequences.reshape(7*4, time_steps, 1))
-    
+    #sequences = np.array(sequences)
+    #print(sequences.reshape(7, time_steps*4, 1))
+    print()
 
     model = Sequential()
+    model.add(LSTM(units=32, input_shape=(data.shape[1], 1)))
     
-    
-
+    model.compile(loss='mse', optimizer='adam')
+    model.fit(data, data)
 
 
 if __name__ == "__main__":
