e0fc312a839c9652c92926663fd9253f1bf7d46a
TPWS2122-MLBCI-MF <moritz.fesseler@student.uni-tuebingen.de>
2023-02-21 19:50:00 +0100

    update

--- Programm_boerse_realtime.py
+++ Programm_boerse_realtime.py
@@ -7,17 +7,42 @@ import os
 import time
 
 def main():
-    begin = dt.date(2022, 12, 1)
-    end = dt.date(2023, 2, 1)
-    ticker_symbols = {'AAPL', 'MSFT', 'AMZN', 'ADBE','AAT','AAU',
-                        'AB','ABBV','ABC','ABCB'}
+    #begin = dt.date(2022, 12, 1)
+    #end = dt.date(2023, 2, 1)
 
-    pair_list = create_key_val_pair(ticker_symbols)
-    collect_data(begin, end, ticker_symbols, pair_list)
 
+    actualize_files()
     
 
-def collect_data(begin, end, ticker_symbols, pair_list : dict):
+
+
+
+
+def actualize_files():
+    begin_date = get_last_date_from_files()
+    end_date = dt.date.today()
+    ticker_symbols = {'AAPL', 'MSFT', 'AMZN', 'ADBE','AAT','AAU',
+                    'AB','ABBV','ABC','ABCB'}
+    pair_list = create_key_val_pair(ticker_symbols)
+
+    download_data_and_write_to_file(begin_date, end_date, ticker_symbols, pair_list)
+
+
+def get_last_date_from_files():
+    file = open('C:/Users/Moritz/Desktop/Allgemeines/MachineLearning/mydata/'+"AAU"+".txt").read()
+    lines = file.split("\n")
+    lines = lines[len(lines)-3].split(";")
+    lines = lines[0].split("-")
+
+    i0 = int(lines[0])
+    i1 = int(lines[1])
+    i2 = int(lines[2])
+    temp = dt.date(i0, i1, i2)
+    temp = temp + dt.timedelta(-1)
+
+    return temp
+
+def download_data_and_write_to_file(begin, end, ticker_symbols, pair_list : dict):
     delta = end - begin
     downloaded_data = np.zeros(shape=[delta.days+1, len(ticker_symbols)])
     downloaded_data[0,:] = list(pair_list.keys())
