a8bcf87e2cc953bcad256c3faf91e9bbbeef48b4
TPWS2122-MLBCI-MF <moritz.fesseler@student.uni-tuebingen.de>
2023-04-07 15:00:59 +0200

    update

--- Programm_boerse_realtime.py
+++ Programm_boerse_realtime.py
@@ -3,14 +3,16 @@ import urllib.request
 import json
 import numpy as np
 import tensorflow as tf
-from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers import Dense, Dropout, LSTM, Embedding
 import keras
 import sys
 import math
 import matplotlib.pyplot as plt
 import os
 from sklearn.preprocessing import normalize
-from UsefulFunctions import HistoryPlotter as HP
+from keras.losses import MeanSquaredError as MSE
+from keras.losses import CategoricalHinge as CH
+from keras.losses import Poisson as PS
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
@@ -19,16 +21,26 @@ def main():
     np.set_printoptions(threshold=sys.maxsize)
     begin = dt.date(2022, 12, 10)
     end = dt.date(2023, 2, 10)
-    vector_size = 8
+    vector_size = 16
     normalize_flag = False
 
   
     ticker_symbols = ['AAPL', 'MSFT', 'AMZN', 'ADBE','AAT','AAU', 'AB','ABBV','ABC','ABCB']
-    #ticker_symbols = ['AAPL', 'MSFT', 'AMZN', 'ADBE', 'AAT']
     pair_list = create_key_val_pair(ticker_symbols)
 
     #actualize_files(ticker_symbols, pair_list)
     [morning_numpy_array, evening_numpy_array] = read_files(ticker_symbols, begin, end)
+
+    
+    
+    [X_train, X_test, Y_train, Y_test] = create_train_test_split_LSTM(morning_numpy_array, ticker_symbols)
+    
+    
+    
+    create_neural_net_and_feed_it_yummy_yummy(X_train, X_test, Y_train, Y_test, vector_size)
+    return
+    
+    
     sequenced_dataset_morning = create_sequence_dataset(morning_numpy_array, vector_size, normalize_flag)
     sequenced_dataset_evening = create_sequence_dataset(evening_numpy_array, vector_size, normalize_flag)
     
@@ -40,54 +52,59 @@ def main():
 
 
 
-
-
 def create_neural_net_and_feed_it_yummy_yummy(X_train : np.array,  X_test : np.array, Y_train : np.array, Y_test : np.array, vector_size):
-    stock_amount = X_train.shape[0]
-    data_length = X_train.shape[1]   
+
     dropout_rate = 0.3
-    #log_base = 2
-    
-    layer_size = 6
-    layer_number = 6
+    layer_size = 8
+    layer_number = 3
+    batch_size_value = 10
+    tensor_var = 32
+    data_vectors = X_train.shape[1]
 
-    
-    early_stopping_callback = keras.callbacks.EarlyStopping(monitor='loss', patience=30, restore_best_weights=True)
-    
 
+    print(X_train.shape)
+    print(X_test.shape)
+    print(Y_train.shape)
+    print(Y_test.shape)
+    print("-----------------")
+    early_stopping_callback = keras.callbacks.EarlyStopping(monitor='loss', patience=5, restore_best_weights=True)
+    
     model = keras.Sequential()
+    
+    model.add(Dense(units = data_vectors, input_shape=(batch_size_value, data_vectors)))
 
-
-    model.add(Dense(units = data_length, input_dim = data_length))
+    model.add(LSTM(units = data_vectors, activation='relu', return_sequences=True, input_shape=[batch_size_value, data_vectors]))
     
     for i in range(0, layer_number):
-        model.add(Dense(units = layer_size, activation='sigmoid'))
-        model.add(Dropout(rate = dropout_rate))
-    
-    model.add(Dense(units = vector_size))
+        model.add(LSTM(units = data_vectors, activation='relu', return_sequences=True, input_shape=[batch_size_value, data_vectors]))
+        
+    model.add(Dense(units = data_vectors, activation='relu'))
+    model.add(Dense(units = 1))
 
+    #kl_divergence, categorical_hinge has different results
+    #log_cosh was best
+    model.compile(loss="mse", optimizer='adam')
     
-    model.compile(loss='mse', optimizer='sgd', metrics='mean_absolute_error')
-    
-    history = model.fit(x=X_train, y=Y_train, batch_size=1, validation_data=(X_test, Y_test), epochs=200, callbacks=[early_stopping_callback])
+    history = model.fit(x=X_train, y=Y_train, validation_data=(X_test, Y_test), epochs=50, callbacks=[early_stopping_callback], batch_size=batch_size_value)
 
     figure, axis = plt.subplots(5, 2)
     my_predictions = []
     
+
+   
+
+    
     
     for i in range(0, Y_test.shape[0]):
-        #print("Output as it should look like: ")
-        #print(Y_test[i])
-        #print("-----------------------------------")
+        print("Output as it should look like: ")
+        print(Y_test[i])
+        print("-----------------------------------")
         X_test_reshaped_line = np.reshape(a=X_test[i], newshape=(1, X_test[i].shape[0]))
 
-        print("X_test[i] shape: ")
-        print(X_test_reshaped_line.shape)
-        print("-----------------------------")
         my_pred = model.predict(X_test_reshaped_line)
         my_predictions.append(my_pred)
         print("Prediction: ")
-        print(my_pred.shape)
+        print(my_pred)
 
         axis[i, 0].plot(Y_test[i])
         axis[i, 0].set_title("Test-set values")
@@ -133,6 +150,38 @@ def create_train_test_split(numpy_dataset : np.array, ticker_symbols : list):
 
     return [X_train, X_test, Y_train, Y_test]
 
+
+
+def create_train_test_split_LSTM(numpy_dataset : np.array, ticker_symbols : list):  
+    split_var = -17
+    target : np.array = numpy_dataset[:][0]
+    input : np.array = numpy_dataset[:][1:]
+    
+    
+    
+    Y_train = target[:split_var]
+    Y_test = target[target.shape[0] + split_var : target.shape[0]]
+
+   
+    X_train : np.array = input[:, :split_var]  
+    X_test = input[:, input.shape[1] + split_var : input.shape[1]]
+
+    #reshaping
+    Y_train = Y_train.reshape(1, Y_train.shape[0])
+    Y_test = Y_test.reshape(1, Y_test.shape[0])
+
+    X_train = np.transpose(X_train)
+    Y_train = np.transpose(Y_train)
+    X_test = np.transpose(X_test)
+    Y_test = np.transpose(Y_test)
+
+    #X_train = X_train.reshape(1, X_train.shape[0], X_train.shape[1])
+    #Y_train = Y_train.reshape(1, Y_train.shape[0], Y_train.shape[1])
+    #X_test = X_test.reshape( X_test.shape[0], X_test.shape[1])
+    #Y_test = Y_test.reshape( Y_test.shape[0], Y_test.shape[1])
+    
+
+    return [X_train, X_test, Y_train, Y_test]
     
 
 
@@ -169,36 +218,39 @@ def read_files(ticker_symbols, begin_date, end_date):
     delta = delta.days
     
     counter = 0
-    #morning_numpy_array = np.empty([len(ticker_symbols), int(delta)])
-    #evening_numpy_array = np.empty([len(ticker_symbols), int(delta)])
     morning_numpy_array = []
     evening_numpy_array = []
-
+    
+    
     for item in ticker_symbols:
         file = open('C:/Users/Moritz/Desktop/Allgemeines/MachineLearning/mydata/'+str(item)+".txt").read()
         lines = file.split("\n")
 
         morning_list = []
         evening_list = []
-        #morning_list.append(str(item))
-        #evening_list.append(str(item))
 
         for line in lines:
-            
-            data = line.split(";")
-
-            if data[0] == '' or data[1] == '' or data[2] == '':
+            if len(line) < 2:
                 continue
 
-            morning_list.append(data[1].replace(" ",""))
-            evening_list.append(data[2].replace(" ",""))
-        
-        morning_numpy_array.append(np.array(morning_list))
-        evening_numpy_array.append(np.array(evening_list))
+            data = line.split(";")
+
+            if data[0] == '' or data[1] == '' or data[2] == '' or data[0].replace(" ","") == 'None' or data[1].replace(" ","") == 'None' or data[2].replace(" ","") == 'None':
+                continue
+            
+            morning_list.append(float(data[1].replace(" ","")))
+            evening_list.append(float(data[2].replace(" ","")))
+
+       
+        morning_numpy_array.append(morning_list)
+        evening_numpy_array.append(evening_list)
 
         counter = counter + 1
 
-    return [np.transpose(np.array(morning_numpy_array)), np.transpose(np.array(evening_numpy_array))]
+    #return [np.transpose(np.array(morning_numpy_array, dtype='object')), np.transpose(np.array(evening_numpy_array, dtype='object'))]
+    return [np.array(morning_numpy_array), np.array(evening_numpy_array)]
+
+    
 
 
 
